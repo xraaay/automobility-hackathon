3184441f1bbb48f94b29eab3ad90358752d6ba10
Beatriz Tovar <beatrizctovar@gmail.com>
2018-11-25 14:27:11 -0800

    installed react-bootstrap

--- reactgm/src/App.js
+++ reactgm/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styles from "./App.module.css";
+import { Modal } from 'react-bootstrap';
 
 const gm = window.gm;
 
@@ -21,7 +22,14 @@ class App extends Component {
     return (
       <div className={styles.root}>
         <div>VIN: {this.state.vin}</div>
+        <div>VIN: {this.state.vin}</div>
         <button onClick={this.handleClose}>Close</button>
+
+        <Modal show={true} close>
+        <Modal.Body>
+          <span style={{ color: 'white', backgroundColor: 'white'}}>sdskjfksdl</span>
+        </Modal.Body>
+        </Modal>
       </div>
     );
   }
--- src/service-worker.js
+++ src/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5f71d82c4af4601c5f53dc841deeae4.js"
+  "/precache-manifest.0312282d3e03d0819af2580b9e78c33b.js"
 );
 
 workbox.clientsClaim();
