58542abe61646ac3ded5870b06130664e4527c1a
Raymond Xie <raymond.y.xie@gmail.com>
2018-11-25 11:13:48 -0800

    second

--- src/service-worker.js
+++ src/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5f71d82c4af4601c5f53dc841deeae4.js"
+  "/precache-manifest.73ab8a7f50aa53bc2739d445735aae14.js"
 );
 
 workbox.clientsClaim();
