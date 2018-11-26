b7df3747fad0404eb2eeb70ee2b2dfb646248de1
Aki Imai <akimai808@gmail.com>
2018-11-25 21:40:20 -0800

    modal

--- reactgm/src/Layout/Layout.js
+++ reactgm/src/Layout/Layout.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ContentRouter from './ContentRouter'
 import { withRouter } from 'react-router-dom'
-
+import { Modal } from 'react-bootstrap'; 
+import "typeface-palanquin-dark";
+const gm = window.gm; 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props) 
+
+        this.state = {
+            odometer: null, 
+            show: null
+        }
+        this.handleClose = this.handleClose.bind(this); 
+    }
+
     redirect = val => {
         switch(val){
             case 1:
@@ -14,12 +26,70 @@ class Layout extends React.Component {
         }
     }
 
+    componentDidMount() {
+        console.log('calling getVehicleData')
+        gm.info.getVehicleData((data) => {
+            console.log('vehicle data', data);
+            this.setState({
+                odometer: data.odometer
+            })
+        }, ['odometer']);
+
+        gm.info.watchVehicleData((data) => {
+            if (data.odometer == 25) {
+                this.setState({
+                    show: true
+                })
+            } 
+            console.log(data)
+        }, ['odometer']);
+    }
+// so if car is in motion, another modal saying that you cannot schedule while car is in motion
+// --> remind me later 
+    
+    handleClose() {
+        this.setState({ show: false });
+    }
+
     render(){
+        if (this.state.show == true) {
+            console.log('hello')
+        } 
         return (
             <React.Fragment>
                 <button type="button" onClick={e => {this.redirect(1)}}>Homepage</button>
                 <button type="button" onClick={e => {this.redirect(2)}}>Test</button>
                 <ContentRouter />
+
+               <Modal show={this.state.show} onHide={this.handleClose} animation={false} style={{ top: "25%" }} backdropStyle={{ opacity: 0.5 }}>
+                    <Modal.Body>
+                        <div className="container" style={{textAlign: "center"}}>
+                            <div className="row" style={{ color: "black" }}>
+                                <i class="fa fa-car"></i>
+                                <h2>Car needs service</h2>
+
+                                <p>Your mileage has reached {this.state.odometer}</p>
+                                <p>Would you like to schedule an appointment?</p>
+                                <div style={{textAlign: "center"}}>
+                                <button type="button" className="btn btn-default" onClick={e => this.handleClose(e)}>Schedule Now</button>
+                                <button type="button" className="btn btn-default" onClick={e => this.handleClose(e)}>No, remind me later</button>
+                                </div>
+                                {/* {select}
+                                
+                                <div style={{ textAlign: "left", padding: "20px", backgroundColor: "white", borderRadius: "10px" }}>
+                                <h4>Timer {ms(this.state.time)}</h4>
+                                {start}
+                                {resume}
+                                {stop}
+                                {reset}
+                                </div> */}
+                            </div>
+                        </div>
+                        <button type="button" onClick={e => this.handleClose(e)}>Test</button>
+
+                    </Modal.Body>
+                </Modal>
+
             </React.Fragment>
         )
     }
--- src/service-worker.js
+++ src/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.010ed01b4bfe45cc803a3b280afcde4e.js"
+  "/precache-manifest.c18b7f6ac1c1edca0f8ac008b4da49e1.js"
 );
 
 workbox.clientsClaim();
