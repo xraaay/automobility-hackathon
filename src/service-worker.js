95821445668f30bdd2616ddc5ad5c58547c86b75
Jaime Ortiz <jxme88@gmail.com>
2018-11-25 20:09:23 -0800

    started vin data branch

--- src/service-worker.js
+++ src/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4690457bc3e9b884854b686bbfb55685.js"
+  "/precache-manifest.2358d447756c99f4758bf5443ad2eebb.js"
 );
 
 workbox.clientsClaim();
